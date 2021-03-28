d321e1ded6224a31665e70ea259ca1e846a5d3d7
miboj <mico.boje@systematic.com>
2021-03-28 15:40:53 +0200

    added histogram and skewness/kortosis measure

--- ArticleReader.py
+++ ArticleReader.py
@@ -3,12 +3,13 @@ import requests
 import nltk # Note that nltk.download() might need to be called to download the required packages
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
+import pandas as pd
 
 class ArticleReader(object):
     def __init__(self):
         self.table = {}
+        self.total_word_frequency = {}
         self.lemmatizer = WordNetLemmatizer()
 
     def data_reader(self, url: str):
@@ -23,15 +24,22 @@ class ArticleReader(object):
     def get_table(self):
         return self.table
 
+    def get_total_freq(self):
+        return self.total_word_frequency
+
+    def clear_table(self):
+        self.table.clear()
+
     def _set_table(self, words: list, docid: int):
         count_dict = Counter(words)
         for key in count_dict.keys():
             if key in self.table:
                 self.table[key].append([docid, count_dict[key]])
+                self.total_word_frequency[key] += count_dict[key]
             else:
                 self.table[key] = [[docid, count_dict[key]]]
+                self.total_word_frequency[key] = count_dict[key]
 
-    #This method could be integrated into data_reader, but for the sake of limiting a method to one function i've split it into two
     @staticmethod
     def _get_html(url: str) -> bytes:
         html = requests.get(url)
@@ -46,5 +54,21 @@ class ArticleReader(object):
 
 a = ArticleReader()
 a.data_reader(url='https://www.uva.nl/binaries/content/assets/programmas/information-studies/txt-for-assignment-data-science.txt')
-#print(list(soup.children))
-print(a.get_table())
+hash_table = a.get_table()
+print(hash_table)
+
+# Plotting frequency distributions
+freq_list = list(a.get_total_freq().values())
+df = pd.DataFrame(freq_list, columns=['Frequency'])
+import plotly.express as px
+fig = px.histogram(df, x='Frequency', histnorm='percent')
+fig.show()
+print('Median: ', df.median())
+print('Mean: ', df.mean())
+print('Mode: ', df.mode())
+
+#calculate skewness
+from scipy.stats import skew, skewtest, kurtosis
+print('Skewness: ', skew(df.Frequency))
+print('Kurtosis: ', kurtosis(df.Frequency))
+
